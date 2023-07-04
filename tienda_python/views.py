5921b2bd358e64e2445865892989c4c189ffdbd4
Luis_Miguel_Gonzalez_Giraldo <miguel.gonzalez@cinnco.co>
2023-07-03 19:27:16 -0500

    Se crean elementos estaticos y codigo necesario para trabajar con interfaces graficas dentro de Django

--- tienda_python/views.py
+++ tienda_python/views.py
@@ -3,6 +3,28 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html', {
-        'title': 'Index',
-        'message': 'Prueba de mensaje.'
+        'title': 'Productos',
+        'message': 'Listado de productos.',
+        'products' : [
+            {
+                'title': 'Playera',
+                'price': 5,
+                'stock': True,
+            },
+            {
+                'title': 'Camisa',
+                'price': 7,
+                'stock': True,
+            },
+            {
+                'title': 'Mochila',
+                'price': 20,
+                'stock': False,
+            },
+            {
+                'title': 'Laptop',
+                'price': 500,
+                'stock': True,
+            },
+        ]
     });
